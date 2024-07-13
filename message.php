3438a330844e7b7879122c57ad2cbd85fce5f05c
muhammedrishadpe <rishadpe@gmail.com>
2024-07-13 14:43:38 +0200

    updated php: merging concating all user values inside body variable.

--- message.php
+++ message.php
@@ -1,7 +1,26 @@
 <?php
 
 
-echo"this msg is send from PHP file";
-
-
-?>
+$name = $_POST['name'];
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$website = $_POST['website'];
+$message = $_POST['message'];
+if (!empty($email) && !empty($message)) {
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) { // if user entered email is valid
+        $receiver = "rishadpe@gmail.com";
+        $subject = "From: $name <$email>";
+        // merging concating all user values inside body variable.
+        $body = "Name: $name\nEmail: $email\nPhone: $website\n\nMessage: $message\n\nRegarda, \n$name";
+        $sender = "From: $email"; //sender email
+        if (mail($receiver, $subject, $body, $sender)) { // mail() is a inbuild php function to send mail
+            echo "Your message has been sent!";
+        } else {
+            echo "Sorry, failed to send your message!";
+        };
+    } else {
+        echo "Enter a valid email address!";
+    }
+} else {
+    echo "Email and password field is required!";
+}
--- script.js
+++ script.js
@@ -1,19 +1,31 @@
-
 const form = document.querySelector("form"),
-statusTxt = form.querySelector(".button-area span");
+  statusTxt = form.querySelector(".button-area span");
 
 form.onsubmit = (e) => {
-    e.preventDefault();
-    statusTxt.style.display  = "block";
-
-    let xhr = new XMLHttpRequest(); // createing new xml object
-    xhr.open("POST", "message.php", true); //  sending post req to message.php file
-    xhr.onload = () => { // once ajax loaded
-if(xhr.readyState == 4 && xhr.status == 200) { // if ajax response status is 200 & ready status is 4 means there is no any error
-    let response = xhr.response; // storing ajax response in a response variable
-    console.log(response);
-
-}
-} 
-xhr.send();
-}
+  e.preventDefault();
+  statusTxt.style.color = "#0D6EFD";
+  statusTxt.style.display = "block";
+  let xhr = new XMLHttpRequest(); // createing new xml object
+  xhr.open("POST", "message.php", true); //  sending post req to message.php file
+  xhr.onload = () => {
+    // once ajax loaded
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      let response = xhr.response; 
+      if (
+        response.indexOf("Email and password field is required!") != -1 ||
+        response.indexOf("Enter a valid email address!") ||
+        response.indexOf("Sorry, failed to send your message!")
+      ) {
+        statusTxt.innerHTML = "red";
+      } else {
+        form.reset();
+        setTimeout(() => {
+          statusTxt.style.display = "none";
+        }, 3000);
+      }
+      statusTxt.innerHTML = response;
+    }
+  };
+  let formData = new FormData(form); 
+  xhr.send(formData); //sending form data
+};
